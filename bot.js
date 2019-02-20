cd03cdf3ad6d81b5fff9c9c394d05cf69e521636
RuthlessNesher <47820140+RuthlessNesher@users.noreply.github.com>
2019-02-20 15:18:52 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,7 @@
+onst roblox = require('roblox-js')
+const Discord = require('discord.js')
+const client = new Discord.Client();
+var token = "NTQ3NzU5MTE1MTg3NzgxNjM0.D07cNw.eQnFZ0F_ozOmtiIfpLLdWC7_6d0";
+
+client.login(token)
+
